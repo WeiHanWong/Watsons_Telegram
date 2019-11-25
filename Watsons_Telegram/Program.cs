b48c3f01c431a0b3f0919c8144e990330ab9960a
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2019-11-25 16:14:41 +0800

    update output

--- Watsons_Telegram/Program.cs
+++ Watsons_Telegram/Program.cs
@@ -41,9 +41,6 @@ namespace Watsons_Telegram
             Bot.OnMessage += BotOnMessageReceived;
             Bot.OnMessageEdited += BotOnMessageReceived;
             Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
-            Bot.OnInlineQuery += BotOnInlineQueryReceived;
-            Bot.OnInlineResultChosen += BotOnChosenInlineResultReceived;
-            Bot.OnReceiveError += BotOnReceiveError;
 
             Bot.StartReceiving(Array.Empty<UpdateType>());
             Console.WriteLine($"Start listening for @{me.Username}");
@@ -65,71 +62,51 @@ namespace Watsons_Telegram
 
                     await Task.Delay(500); // simulate longer running task
 
-                    var inlineKeyboard = new InlineKeyboardMarkup(new[]
-                    {
-                        new [] // first row
-                        {
-                            InlineKeyboardButton.WithCallbackData("1.1"),
-                            InlineKeyboardButton.WithCallbackData("1.2"),
-                        },
-                        new [] // second row
-                        {
-                            InlineKeyboardButton.WithCallbackData("2.1"),
-                            InlineKeyboardButton.WithCallbackData("2.2"),
-                        }
-                    });
-
                     await Bot.SendTextMessageAsync(
                         message.Chat.Id,
-                        "not so Available");//,
-                                            //replyMarkup: inlineKeyboard);
+                        "Available");
                     break;
 
                 // send custom keyboard
                 case "/waitingTime":
-                    ReplyKeyboardMarkup ReplyKeyboard = new[]
-                    {
-                        new[] { "1.1", "1.2" },
-                        new[] { "2.1", "2.2" },
-                    };
+                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+                    await Task.Delay(500); // simulate longer running task
 
                     await Bot.SendTextMessageAsync(
                         message.Chat.Id,
-                        "10h 10m");//,
-                                   //replyMarkup: ReplyKeyboard);
+                        "10mins");
                     break;
 
                 // send a photo
                 case "/queueLength":
-                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
 
-                    const string file = @"Files/tux.png";
+                    await Task.Delay(500); // simulate longer running task
 
-                    var fileName = file.Split(Path.DirectorySeparatorChar).Last();
-
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "xiao long");
                     break;
 
                 // request location or contact
                 case "/operatingHours":
-                    var RequestReplyKeyboard = new ReplyKeyboardMarkup(new[]
-                    {
-                        KeyboardButton.WithRequestLocation("Location"),
-                        KeyboardButton.WithRequestContact("Contact"),
-                    });
+                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+                    await Task.Delay(500); // simulate longer running task
 
                     await Bot.SendTextMessageAsync(
                         message.Chat.Id,
-                        "Who or Where are you?",
-                        replyMarkup: RequestReplyKeyboard);
+                        "8:00am to 11:45pm");
                     break;
 
                 default:
                     const string usage = @"
-Usage:
-/slotAvailability   - get slot availability
-/waitingTime - get average waiting time
-/queueLength    - get queue length
-/operatingHours  - get operating hours";
+                        Usage:
+                        /slotAvailability   - get slot availability
+                        /waitingTime - get average waiting time
+                        /queueLength    - get queue length
+                        /operatingHours  - get operating hours";
 
                     await Bot.SendTextMessageAsync(
                         message.Chat.Id,
@@ -150,53 +127,6 @@ Usage:
             await Bot.SendTextMessageAsync(
                 callbackQuery.Message.Chat.Id,
                 $"Received {callbackQuery.Data}");
-        }
-
-        private static async void BotOnInlineQueryReceived(object sender, InlineQueryEventArgs inlineQueryEventArgs)
-        {
-            Console.WriteLine($"Received inline query from: {inlineQueryEventArgs.InlineQuery.From.Id}");
-
-            InlineQueryResultBase[] results = {
-                new InlineQueryResultLocation(
-                    id: "1",
-                    latitude: 40.7058316f,
-                    longitude: -74.2581888f,
-                    title: "New York")   // displayed result
-                    {
-                        InputMessageContent = new InputLocationMessageContent(
-                            latitude: 40.7058316f,
-                            longitude: -74.2581888f)    // message if result is selected
-                    },
-
-                new InlineQueryResultLocation(
-                    id: "2",
-                    latitude: 13.1449577f,
-                    longitude: 52.507629f,
-                    title: "Berlin") // displayed result
-                    {
-                        InputMessageContent = new InputLocationMessageContent(
-                            latitude: 13.1449577f,
-                            longitude: 52.507629f)   // message if result is selected
-                    }
-            };
-
-            await Bot.AnswerInlineQueryAsync(
-                inlineQueryEventArgs.InlineQuery.Id,
-                results,
-                isPersonal: true,
-                cacheTime: 0);
-        }
-
-        private static void BotOnChosenInlineResultReceived(object sender, ChosenInlineResultEventArgs chosenInlineResultEventArgs)
-        {
-            Console.WriteLine($"Received inline result: {chosenInlineResultEventArgs.ChosenInlineResult.ResultId}");
-        }
-
-        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
-        {
-            Console.WriteLine("Received error: {0} — {1}",
-                receiveErrorEventArgs.ApiRequestException.ErrorCode,
-                receiveErrorEventArgs.ApiRequestException.Message);
         }
     }
 }
