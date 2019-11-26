08c2308bec1cf0c098ed362c3c46777415f1a31f
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2019-11-26 12:13:59 +0800

    added other message handler

---
+++ Watsons_Telegram/Models/Commands/OtherCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Watsons_Telegram.Models.Commands
+{
+    public class OtherCommand : Command
+    {
+        public override string Name => "";
+
+        public override bool Contains(Message message)
+        {
+            return true;
+        }
+
+        public override async Task Execute(Message message, TelegramBotClient botClient)
+        {
+            var chatId = message.Chat.Id;
+            await botClient.SendTextMessageAsync(chatId,
+                @"Usage: 
+/slotavailability - get slot availability
+/waitingtime - get average waiting time
+/operatinghours - get operating hours", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+        }
+    }
+}
