ba62bffdf97f20c656487cb38ef12e60f9a9125b
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2019-11-26 11:26:16 +0800

    new

---
+++ Watsons_Telegram/Controllers/MessageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Watsons_Telegram.Models;
+using Telegram.Bot.Types;
+
+namespace Watsons_Telegram.Controllers
+{
+    [Route("api/message/update")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Method GET unavalable";
+        }
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody]Update update)
+        {
+            if (update == null) return Ok();
+
+            var commands = Bot.Commands;
+            var message = update.Message;
+            var botClient = await Bot.GetBotClientAsync();
+
+            foreach (var command in commands)
+            {
+                if (command.Contains(message))
+                {
+                    await command.Execute(message, botClient);
+                    break;
+                }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Watsons_Telegram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Watsons_Telegram
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
