2e0b0c8580d830eeeb517335d6b8452c5886fa2f
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2019-11-27 22:20:04 +0800

    edit

--- Watsons_Telegram/Commands/StartCommand.cs
+++ Watsons_Telegram/Commands/StartCommand.cs
@@ -41,8 +41,8 @@ namespace Watsons_Telegram.Commands
                         new[] { "Operating Hours"},
                     };
 
-            await botClient.SendTextMessageAsync(chatId, @"Registering...
-(Do not type anything. Please wait...)", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown, replyMarkup: ReplyKeyboard);
+            await botClient.SendTextMessageAsync(chatId, @"Verifying...
+(Do not type anything. This make take a few moments. Please wait...)", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown, replyMarkup: ReplyKeyboard);
 
             TelegramUser newUser = new TelegramUser();
 
@@ -52,17 +52,31 @@ namespace Watsons_Telegram.Commands
 
             if (slotQueryResultCount < 1)
             {
+                await botClient.SendTextMessageAsync(chatId, @"Registering...
+(Do not type anything. Please wait...)", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown, replyMarkup: ReplyKeyboard);
+
                 newUser.ChatId = chatId.ToString();
 
                 Database.TelegramUsers.Add(newUser);
                 Database.SaveChanges();
-            }
-
-            await botClient.SendTextMessageAsync(chatId, 
-                @"Usage: 
+                await botClient.SendTextMessageAsync(chatId,
+                @"Registration Completed!
+Usage: 
 /slotavailability - get slot availability
 /waitingtime - get average waiting time
 /operatinghours - get operating hours", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown, replyMarkup: ReplyKeyboard);
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(chatId,
+                @"You had previously registered!
+Usage: 
+/slotavailability - get slot availability
+/waitingtime - get average waiting time
+/operatinghours - get operating hours", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown, replyMarkup: ReplyKeyboard);
+            }
+
+            
         }
     }
 }
--- Watsons_Telegram/Models/Bot.cs
+++ Watsons_Telegram/Models/Bot.cs
@@ -8,7 +8,7 @@ namespace Watsons_Telegram.Models
     public class Bot
     {
         public static string Url { get; set; } = "https://watsonstelegram.azurewebsites.net:443/{0}";
-        public static string Name { get; set; } = "WatsonsDC_bot";
-        public static string Key { get; set; } = "966338301:AAE0UUHvUFHgmQsNN-J-3YCXZsTjOkooeyc";
+        public static string Name { get; set; } = "watsonsDC_test_bot";
+        public static string Key { get; set; } = "1063401673:AAGwiUuioG-7Hec9V0ad4sp_AxjkofcoHpU";
     }
 }
